aad7392defe73c3d45908124b4a468a1395dda13
sas0501 <sas0501@mail.ru>
2021-11-02 18:07:27 +0300

    lesson3

--- src/com/company/HandlerSax.java
+++ src/com/company/HandlerSax.java
@@ -1,12 +1,17 @@
 package com.company;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-    public class HandlerSax extends DefaultHandler {
-
-        private StringBuilder currentValue = new StringBuilder();
-        String file;
+public class HandlerSax extends DefaultHandler {
+    String name = null;
+    Boolean bChildren = false;
+    Boolean bChild = false;
+    String child = null;
+    String slash;
+    private StringBuilder currentValue = new StringBuilder();
+    private StringBuilder stroka = new StringBuilder();
 //        @Override
 //        public void startDocument() {
 //            System.out.println("Start Document");
@@ -17,84 +22,72 @@ import org.xml.sax.helpers.DefaultHandler;
 //            System.out.println("End Document");
 //        }
 
-        @Override
-        public void startElement(String uri, String localName, String qName, Attributes attributes) {
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
 
-            // reset the tag value
-            currentValue.setLength(0);
-
-//                if (qName.equalsIgnoreCase("child")&& attributes.getValue("is-file")=="true") {
-                if (qName.equalsIgnoreCase("child")) {
-                file= attributes.getValue("is-file");
-                }
-
-//                    System.out.println("%s%n",qName);
+        // reset the tag value
+        currentValue.setLength(0);
 
 
-
-                // get tag's attribute by index, 0 = first attribute
-//            String currency = attributes.getValue(0);
-//            System.out.printf("Name :%s%n", attributes.getValue(0));
-
-
-//            System.out.printf("Start Element : %s%n", qName);
-
-//            if (qName.equalsIgnoreCase("child")) {
-//                // get tag's attribute by name
-//                String id = attributes.getValue("is-file");
-//                System.out.printf("Child is-file: %s%n", attributes.getValue("is-file"));
-//            }
-
-
-
+        if (qName.equalsIgnoreCase("children") && bChildren == false) {
+            bChildren = true;
+//                System.out.println(bChildren);
+        } else if ((qName.equalsIgnoreCase("children") && bChildren == true)) {
+//                System.out.println(bChildren);
         }
 
-        @Override
-        public void endElement(String uri,
-                               String localName,
-                               String qName) {
-
-//            System.out.printf("End Element : %s%n", qName);
-
-//            if (qName.equalsIgnoreCase("name")) {
-//                System.out.printf("Name : %s%n", currentValue.toString());
-//            }
-//
-//            if (qName.equalsIgnoreCase("role")) {
-//                System.out.printf("Role : %s%n", currentValue.toString());
-//            }
-
-            if (qName.equalsIgnoreCase("name")) {
-                System.out.printf("%s", currentValue.toString());
-
-            }
-
-            if ((qName.equalsIgnoreCase("name"))&& (file=="true")) {
-                System.out.printf("Название : %s", currentValue.toString());
-                file="false";
-            }
-//
-//            if (qName.equalsIgnoreCase("bio")) {
-//                System.out.printf("Bio : %s%n", currentValue.toString());
-//            }
-
-        }
-
-        // http://www.saxproject.org/apidoc/org/xml/sax/ContentHandler.html#characters%28char%5B%5D,%20int,%20int%29
-        // SAX parsers may return all contiguous character data in a single chunk,
-        // or they may split it into several chunks
-        @Override
-        public void characters(char ch[], int start, int length) {
-
-            // The characters() method can be called multiple times for a single text node.
-            // Some values may missing if assign to a new string
-
-            // avoid doing this
-            // value = new String(ch, start, length);
-
-            // better append it, works for single or multiple calls
-            currentValue.append(ch, start, length);
-
+        if (qName.equalsIgnoreCase("child") && attributes.getValue("is-file").equals("true")) {
+            bChild = true;
+//                System.out.println(bChild);
+        } else if (qName.equalsIgnoreCase("child") && attributes.getValue("is-file").equals("false")) {
+            bChild = false;
+//                System.out.println(bChild);
         }
 
     }
+
+
+    @Override
+    public void endElement(String uri,
+                           String localName,
+                           String qName) {
+
+        if (qName.equalsIgnoreCase("children") && bChildren == true) {
+            bChildren = false;
+//                System.out.println(bChildren);
+        } else if ((qName.equalsIgnoreCase("children") && bChildren == false)) {
+//                System.out.println(bChildren);
+        }
+
+
+        if (qName.equalsIgnoreCase("name")) {
+            if (currentValue.toString().equals("/")) {
+                slash = currentValue.toString();
+            }
+//            stroka.append(slash);
+            if (qName.equalsIgnoreCase("name") && bChildren == true && bChild == true) {
+                stroka.append(slash);
+
+                Integer nacalo = stroka.length();
+
+                stroka.append(currentValue);
+
+                Integer konec = stroka.length();
+                System.out.printf("Название : %s%n", stroka.toString());
+                stroka.delete(nacalo-1, konec);
+            } else if (qName.equalsIgnoreCase("name") && bChildren == true && bChild == false) {
+                stroka.append(slash);
+                stroka.append(currentValue);
+            }
+
+        }
+    }
+
+    @Override
+    public void characters(char ch[], int start, int length) {
+
+        currentValue.append(ch, start, length);
+
+    }
+
+}
