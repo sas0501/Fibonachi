ceebd84786e7a89d6cafd753ca5b5ff309073315
sas0501 <sas0501@mail.ru>
2022-03-02 23:08:56 +0300

    NEW 02.03.20122

--- Fibonachi.java
+++ Fibonachi.java
@@ -3,6 +3,7 @@ package com.fibonachi;
 import java.math.BigInteger;
 
 public class Fibonachi {
+    static BigInteger[] fibCache = new BigInteger[10000];
 
     public static void main(String[] args) {
         // Fibonacci v cikle
@@ -18,13 +19,22 @@ public class Fibonachi {
         }
         // Rekursia
         System.out.println();
-        BigInteger num=BigInteger.valueOf(0);
+        BigInteger num = BigInteger.valueOf(0);
         BigInteger i = BigInteger.valueOf(20);
-        for (; num.compareTo(i) <= 0;) {
-            num=num.add(BigInteger.ONE);
+        for (; num.compareTo(i) <= 0; ) {
+            num = num.add(BigInteger.ONE);
             System.out.print(fibonachi(num) + " ");
         }
+
+        //KASH
+        System.out.println();
+        int index;
+        for (int k = 0; k < 1000; k++) {
+            index = k;
+            System.out.print(fibonacci1(index) + " ");
+        }
     }
+
     public static BigInteger fibonachi(BigInteger num) {
 
         if (num.compareTo(BigInteger.ONE) <= 0) {
@@ -35,4 +45,26 @@ public class Fibonachi {
             return fibonachi(num.subtract(BigInteger.ONE)).add(fibonachi(num.subtract(BigInteger.TWO)));
         }
     }
+
+    private static BigInteger fibonacci1(int index) {
+        BigInteger result;
+        if (index == 0) {
+            fibCache[index] = BigInteger.valueOf(0);
+            return BigInteger.ZERO;
+        } else if (index == 1) {
+            fibCache[index] = BigInteger.valueOf(1);
+            return BigInteger.ONE;
+        }
+        else {
+            result = fibCache[(index - 1)].add(fibCache[(index - 2)]);
+            fibCache[index] = result;
+            return result;
+        }
+    }
 }
+
+
+
+
+
+
